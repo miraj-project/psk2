057b025664ff431f5162940978d3ace54bea6b07
Gregg Reynolds <reynolds-gregg@norc.org>
2017-07-28 18:09:39 -0500

    make it work
    Signed-off-by: Gregg Reynolds <reynolds-gregg@norc.org>

---
+++ src/foo/delegate.cljs
@@ -0,0 +1,8 @@
+(ns foo.delegate)
+;  (:require [my-app :as foo]))
+
+(enable-console-print!)
+
+(println "hello from delegate!")
+
+(defn foobar [] (println "FOOBAR"))
--- src/my_app/delegate.cljs
+++
@@ -1,8 +0,0 @@
-(ns my-app.delegate)
-;  (:require [my-app :as foo]))
-
-(enable-console-print!)
-
-(println "hello")
-
-(defn foobar [] "FOOBAR")
